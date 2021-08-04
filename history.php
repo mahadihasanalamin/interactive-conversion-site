cef72a9b21b3d25cc193f19f7f3bb00389e0058e
mahadihasanalamin <mahadi.aalamin@gmail.com>
2021-08-04 11:32:02 +0600

    12th commit

--- history.php
+++ history.php
@@ -20,7 +20,26 @@
 	<br>
 	<input id="search" type="text" name="search" placeholder="search by category">
 	<input type="submit" name="submit" value="Submit">
-	<div id="history"></div>
+	<div id="history">
+		<?php
+		include 'file-io.php';
+		$fileData = read();
+			$data= json_decode($fileData);
+	if(empty($data)){
+		echo "No data found!";
+
+	}
+	else{
+
+		echo "<ol>";
+
+	for ($i=0; $i < sizeof($data); $i++) { 
+		echo "<li>" . $data[$i]->conversion ." ". $data[$i]->value ." ". $data[$i]->result . "</li>";
+	}
+	echo"</ol>";
+	}
+		?>
+	</div>
 	</fieldset>
 </form>
 
